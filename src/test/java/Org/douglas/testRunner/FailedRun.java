47fa77af695ad36b973b5059431b619601a871de
dntinfotech12345 <dnt.katalon@gmail.com>
2025-02-19 12:00:04 +0530

    Rerun failed test case mechanism

--- src/test/java/Org/douglas/testRunner/FailedRun.java
+++ src/test/java/Org/douglas/testRunner/FailedRun.java
@@ -5,16 +5,15 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-        features = {"src/test/resources/features/parfumPage.feature"},
+        features = {"@target/rerun.txt"},
         glue = {"Org.douglas.stepDefinitions", "Org.douglas.hooks"},
         plugin = {"pretty", "html:target/cucumber-reports/cucumber-pretty",
                 "json:target/cucumber-reports/CucumberTestReport.json",
-                "rerun:target/rerun.txt",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-        dryRun = false, monochrome = true
+        monochrome = true
 
 )
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class FailedRun extends AbstractTestNGCucumberTests {
 
 }
 
